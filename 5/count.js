d9253362546757f95447cac139984c7d3f1a5dfe
mariachernaya <5chernaya5.5@gmail.com>
2024-10-06 20:15:54 +0300

    Update count.js

--- 5/count.js
+++ 5/count.js
@@ -1,10 +1,20 @@
 window.addEventListener('DOMContentLoaded', function () {
- console.log("DOM fully loaded and parsed");
+  const price =
+    {"pr1": 10, "pr2": 20, "pr3": 30, "pr4": 40, "pr5": 50};
+ 
   let b = document.getElementById("my-button");
   let product = document.getElementById("list");
   let count = document.getElementById("count");
   let r = document.getElementById("result");
-  count.addEventListener("click", function);
+  count.addEventListener("click", function () {
+        let inp = amount.value;
+        if (inp.match(/^\d+$/) !== null) {
+            let result = priceList[product.value] * inp;
+            r.innerText = result;
+        } else {
+            resultfield.innerText = "неверный формат ввода";
+        }
+    });
 });
 
 
