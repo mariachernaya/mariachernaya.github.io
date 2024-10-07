c0cfda6953853c7704ef680942c4a9def32d8970
mariachernaya <5chernaya5.5@gmail.com>
2024-10-07 11:13:36 +0300

    Update count.js

--- 5/count.js
+++ 5/count.js
@@ -1,13 +1,9 @@
 function click1() {
- let f1 = document.getElementsByName("field1");
- let r = document.getElementById("result");
- let s = document.getElementByName("select1");
- if (s[0].value=="v1") r.innerHTML = f1[0].value*3500;
- if (s[0].value=="v2") r.innerHTML = f1[0].value*9000;
- if (s[0].value=="v3") r.innerHTML = f1[0].value*11500;
- if (s[0].value=="v4") r.innerHTML = f1[0].value*8000;
- if (s[0].value=="v5") r.innerHTML = f1[0].value*4000;
- if (r[0].value==null) r.innerHTML ="не корректное значение";
-  console.log(r[0].value);
- return false;
+  let f1 = document.getElementsByName("field1");
+  let f2 = document.getElementsByName("field2");
+  let r = document.getElementById("result");
+  r.innerHTML = f1[0].value + f2[0].value;
+  let s = document.getElementsByName("select1");
+  console.log(s[0].value);
+  return false;
 }
