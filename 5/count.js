1010130db6aa4a72409970a6f87515e7da42ef88
mariachernaya <5chernaya5.5@gmail.com>
2024-10-07 12:22:42 +0300

    Update count.js

--- 5/count.js
+++ 5/count.js
@@ -2,15 +2,15 @@ function click1(event) {
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
   let s = document.getElementsByName("select1");
-  if (f1.match(/^\d+$/)!== null){
+  let z=f1[0].value;
   if (s[0].value=="v1")  r.innerHTML = f1[0].value*3500;
   if (s[0].value=="v2")  r.innerHTML = f1[0].value*7500;
   if (s[0].value=="v3")  r.innerHTML = f1[0].value*11500;
   if (s[0].value=="v4")  r.innerHTML = f1[0].value*8000;
   if (s[0].value=="v5")  r.innerHTML = f1[0].value*4000;
-}
+  if (z.match(/^\d+$/) === null)  r.innerHTML='eror';
+  event.preventDefault(); 
   return false;  
-  event.preventDefault();
 }
 window.addEventListener('DOMContentLoaded', function (event) {
   
