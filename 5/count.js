d6f33b9eba04149bea3156f6507e1395deba3882
mariachernaya <5chernaya5.5@gmail.com>
2024-10-07 12:15:36 +0300

    Update count.js

--- 5/count.js
+++ 5/count.js
@@ -2,12 +2,13 @@ function click1(event) {
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
   let s = document.getElementsByName("select1");
+  if (f1.match(/^\d+$/)!== null){
   if (s[0].value=="v1")  r.innerHTML = f1[0].value*3500;
   if (s[0].value=="v2")  r.innerHTML = f1[0].value*7500;
   if (s[0].value=="v3")  r.innerHTML = f1[0].value*11500;
   if (s[0].value=="v4")  r.innerHTML = f1[0].value*8000;
   if (s[0].value=="v5")  r.innerHTML = f1[0].value*4000;
-  
+  }else  r.innerHTML = "eror";
   return false;  
   event.preventDefault();
 }
