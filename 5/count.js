7dbfd42ddef606b0ea1189e83fff2d30817ad945
mariachernaya <5chernaya5.5@gmail.com>
2024-10-07 12:26:09 +0300

    Update count.js

--- 5/count.js
+++ 5/count.js
@@ -2,7 +2,7 @@ function click1(event) {
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
   let s = document.getElementsByName("select1");
-  let z=f1[0].value;
+  let z = f1[0].value;
   if (s[0].value=="v1")  r.innerHTML = f1[0].value*3500;
   if (s[0].value=="v2")  r.innerHTML = f1[0].value*7500;
   if (s[0].value=="v3")  r.innerHTML = f1[0].value*11500;
