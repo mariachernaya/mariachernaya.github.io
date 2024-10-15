eb8bc6a3fc3f67651d5b5d8c39cffe8ff0e037a6
mariachernaya <5chernaya5.5@gmail.com>
2024-10-15 11:59:44 +0300

    Update count.js

--- 6/count.js
+++ 6/count.js
@@ -1,1 +1,24 @@
+window.addEventListener('DOMContentLoaded', function (event) {
+ let s = document.getElementsByName("myselect");
+ s[0].addEventListener("change", function(event) {
+ let select = event.target;
+ let radios = document.getElementById("myradios");
+ console.log(select.value);
+ // Можно использовать getElementsByClassName()
+ if (select.value == "3") {
+ radios.style.display = "none";
+ }
+ else {
+ radios.style.display = "block";
+ }
+ });
 
+ let r = document.querySelectorAll(".myradios input[type=radio]");
+ r.forEach(function(radio) {
+ radio.addEventListener("change", function(event) {
+ let r = event.target;
+ console.log(r.value);
+ });
+ });
+
+});
