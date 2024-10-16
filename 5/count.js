39e8a91b564d4f168c043baeaa7b685dae763950
mariachernaya <5chernaya5.5@gmail.com>
2024-10-16 14:02:39 +0300

    Update count.js

--- 5/count.js
+++ 5/count.js
@@ -3,14 +3,26 @@ function click1(event) {
     let r = document.getElementById("result");
     let s = document.getElementsByName("select1");
     let z = f1[0].value;
-  if (s[0].value=="v1")  r.innerHTML = f1[0].value*3500;
-  if (s[0].value=="v2")  r.innerHTML = f1[0].value*7500;
-  if (s[0].value=="v3")  r.innerHTML = f1[0].value*11500;
-  if (s[0].value=="v4")  r.innerHTML = f1[0].value*8000;
-  if (s[0].value=="v5")  r.innerHTML = f1[0].value*4000;
-  if (z.match(/^\d+$/) === null)  r.innerHTML='eror';
-  event.preventDefault(); 
-  return false;  
+    if (s[0].value === "v1") {
+        r.innerHTML = f1[0].value * 3500;
+    }
+    if (s[0].valu =e== "v2") {
+        r.innerHTML = f1[0].value * 7500;
+    }
+    if (s[0].value === "v3") {
+        r.innerHTML = f1[0].value * 11500;
+    }
+    if (s[0].value === "v4") {
+        r.innerHTML = f1[0].value * 8000;
+    }
+    if (s[0].value === "v5") {  
+        r.innerHTML = f1[0].value * 4000;
+    }
+    if (z.match(/^\d+$/) === null) {
+        r.innerHTML='eror';
+    }
+    event.preventDefault(); 
+    return false;  
 }
 window.addEventListener('DOMContentLoaded', function (event) {  
   let b = document.getElementById("button1");
