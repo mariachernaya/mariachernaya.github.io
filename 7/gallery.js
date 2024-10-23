1dc837f61f3d55a7e0bf55e394ca7b0184a6c831
mariachernaya <5chernaya5.5@gmail.com>
2024-10-23 16:04:16 +0300

    Update gallery.js

--- 7/gallery.js
+++ 7/gallery.js
@@ -1,1 +1,16 @@
-
+/*jslint browser: true*/ /*global  $*/
+$(document).ready(function() {
+    $(".carousel").slick({
+        dots: true,
+        infinite: true,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 1
+            }
+            }
+        ],
+        slidesToShow: 3
+    });
+});
